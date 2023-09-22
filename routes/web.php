60de81d4ac4c8301d0ee37d5d679a3a3180cfd19
Elnaz <kz_elnaz@yahoo.com>
2023-09-23 03:23:56 +0330

    the project elements (database, tables, controllers, models) are created, and a simple authentication has been implemented (login and registration forms)

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\WilAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Auth routes
+Route::get('dashboard', [WilAuthController::class, 'dashboard']);
+Route::get('login', [WilAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [WilAuthController::class, 'customLogin'])->name('login.custom');
+Route::get('registration', [WilAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [WilAuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('signout', [WilAuthController::class, 'signOut'])->name('signout');
+
+// Default home route
+Route::get('/', [WilAuthController::class, 'index']);
